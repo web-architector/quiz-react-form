e576de510472f548262308471cc7bdaaf5932678
web-architector <webarchitector.spb@gmail.com>
2020-11-02 14:15:42 +0300

    Add docs folder for gh-pages deploy

---
+++ docs/precache-manifest.a76ad8bd7e01a300ec50c7ef09019d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27207b234cf4155b286efe57e39eb7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7eb93fdbd4d4a1ffd3a7",
+    "url": "./static/css/main.091482c7.chunk.css"
+  },
+  {
+    "revision": "5669976675ee28db7cbb",
+    "url": "./static/js/2.839bec46.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "./static/js/2.839bec46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb93fdbd4d4a1ffd3a7",
+    "url": "./static/js/main.1125ae1a.chunk.js"
+  },
+  {
+    "revision": "ae490d7724f7086c5a99",
+    "url": "./static/js/runtime-main.dc0f83e5.js"
+  }
+]);
